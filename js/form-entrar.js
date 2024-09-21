ec2b8e710b7d241d3e2d95d8e703e78117ddb741
EstevaoMO <estevaomoraes48@gmail.com>
2024-09-21 19:35:08 -0300

    Tentando salvar informaões do formulário

---
+++ js/form-entrar.js
@@ -0,0 +1,10 @@
+let inpEntrar = document.querySelector('#botao_entrar');
+
+inpEntrar.addEventListener("click", function (event){
+    event.preventDefault();
+    let formUsuario = document.querySelector("#formuario-entrar");
+    console.log(formUsuario.nome-usuario.value);
+})
+
+
+/* Salvar nome do usuário - pendente */
